f01b8f08a34f3b235cdce75f3637d64a62e188fb
Seth Nielson <seth@crimsonvista.com>
2019-12-05 23:26:07 -0500

    trying to fix pre-commit failures stopping the commit. Fix a few typos

--- git_hooks/top_level_pre-commit.py
+++ git_hooks/top_level_pre-commit.py
@@ -3,10 +3,10 @@ import os, sys
 
 cur_path = os.path.dirname(__file__)
 git_hooks_path = os.path.join(cur_path,"..","..","git_hooks","pre_commit")
-os.chdir(git_hook_path)
+os.chdir(git_hooks_path)
 for hook_file in os.listdir("."):
     print("Run hook {}".format(hook_file))
-	result = os.system("./{}".format(hook_file))
-    print("Exit status = {}".format(result))
-	if result != 0:
-		sys.exit(result)
+    result = os.system("./{}".format(hook_file))
+    print("Exit status={}".format(result))
+    if result != 0:
+        sys.exit(result)
